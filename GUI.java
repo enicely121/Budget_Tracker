da6d8d7497689e2e7b8f1249ff46074e1952ed3a
enicely121 <enicely121@gmail.com>
2025-03-01 17:10:13 -0500

    start

---
+++ GUI.java
@@ -0,0 +1,20 @@
+package Budget;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+public class GUI extends JFrame {
+
+    public GUI() {
+
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+    }
+    public static void main(String[] args) {
+        new GUI
+
+
+    }
+}
---
+++ Main_FUNCTIONALITY.java
@@ -0,0 +1,23 @@
+package Budget;
+/**
+ * Main Functionality class for project
+ *
+ * @author Ethan Nicely
+ * @version 03/1/25
+ */
+
+// use a map with a int key and a int list of values probably a hash map
+// check your prevchat reposnses to see how to use localstorage and json files
+
+public class Main_FUNCTIONALITY{
+    private int weeklyLimit = 30;
+
+
+
+public void addToLimit(int value){
+
+
+}
+
+
+}
